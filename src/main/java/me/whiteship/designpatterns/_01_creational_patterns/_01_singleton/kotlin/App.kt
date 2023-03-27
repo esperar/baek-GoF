b3faa46a80cc82afc39749ade39227b384b4748a
aesperer <piyrw9754@gmail.com>
2023-03-27 10:15:10 +0900

    역직렬화 직렬화 대응

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/kotlin/App.kt
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/kotlin/App.kt
@@ -5,4 +5,6 @@ fun main(){
     val instance = Settings.getInstance()
     val instance2 = Settings.getInstance()
     // instance === instance2 true
+
+    // 싱글턴을 구현해도 리플렉션, 직렬화 역직렬화로 싱글턴을 깨버릴 수 있다.. ㄸ
 }
--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/kotlin/Settings4.kt
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/kotlin/Settings4.kt
@@ -1,12 +1,16 @@
 package me.whiteship.designpatterns._01_creational_patterns._01_singleton.kotlin
 
+
+
 class Settings4 private constructor() {
+    private object Settings4Holder {
+        val INSTANCE = Settings4()
+    }
 
     companion object {
-        val instance: Settings4 by lazy { SettingsHolder.SETTINGS } // by lazy를 통해 객체가 필요한 시점까지 초기화 지연
+        @JvmStatic
+        fun getInstance(): Settings4 = Settings4Holder.INSTANCE
     }
 
-    private object SettingsHolder {
-        val SETTINGS = Settings4()
-    }
+    protected fun readResolve() = getInstance() // 역 직렬화 대응
 }
