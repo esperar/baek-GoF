c63ebfb6bc638da514ad5104816b1eccad3390f2
aesperer <piyrw9754@gmail.com>
2023-03-27 09:29:06 +0900

    instance main

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/kotlin/App.kt
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/kotlin/App.kt
@@ -1,5 +1,8 @@
 package me.whiteship.designpatterns._01_creational_patterns._01_singleton.kotlin
 
-fun main(){
 
+fun main(){
+    val instance = Settings.getInstance()
+    val instance2 = Settings.getInstance()
+    // instance === instance2 true
 }
