fb5fffaa6bb00eca7a26bf6924e7a4a116efb817
aesperer <piyrw9754@gmail.com>
2023-03-27 10:02:15 +0900

    지연 초기화 lazy

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/kotlin/Settings4.kt
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/kotlin/Settings4.kt
@@ -3,7 +3,7 @@ package me.whiteship.designpatterns._01_creational_patterns._01_singleton.kotlin
 class Settings4 private constructor() {
 
     companion object {
-        val instance: Settings4 by lazy { SettingsHolder.SETTINGS }
+        val instance: Settings4 by lazy { SettingsHolder.SETTINGS } // by lazy를 통해 객체가 필요한 시점까지 초기화 지연
     }
 
     private object SettingsHolder {
